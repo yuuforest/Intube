d732750deea8fd37ee71e92d7f22023112460e6c
eosun77 <eosun77@gmail.com>
2023-01-19 12:56:46 +0900

    feat : frontend 초기화

---
+++ frontend/src/views/MainView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <mian-header></mian-header>
+  </div>
+</template>
+
+<script>
+import mianHeader from "@/views/common/TheHeader.vue";
+export default {
+  components: { mianHeader },
+};
+</script>
+
+<style lang="scss" scoped></style>
