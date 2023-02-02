d3d5cdad14709f95a3ee2a0265bf0b186d65cbf1
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-02-02 16:36:05 +0900

    <feat> Tab bar 추가

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import SignInSide from "./mainpage/SignInSide.jsx";
+import InterviewAll from "./mainpage/InterviewAll.jsx";
 
 export default function Main() {
   return (
-  <div>
+  <div
+  style={{margin:'40px 100px'}}
+  >
     {/* <br />
     <br /> */}
     <SignInSide />
+    <br />
+    <br />
+    <InterviewAll />
   </div> 
   );
 
