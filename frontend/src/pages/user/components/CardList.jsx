01c5334688697452667159c54f40e7101c9683cd
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-02-02 10:03:28 +0900

    <feat>: 마이페이지에서 공고  navigate 구현

--- frontend/src/pages/user/components/CardList.jsx
+++ frontend/src/pages/user/components/CardList.jsx
@@ -6,20 +6,20 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function BasicCard() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // const goToMatch = () => {
-  //   navigate("/interview");
-  // };
-  // const goToApply = () => {
-  //   navigate("/interview");
-  // };
-  // const goToClose = () => {
-  //   navigate("/interview");
-  // };
+  const goToMatch = () => {
+    navigate("/interview", {state: '2'});
+  };
+  const goToApply = () => {
+    navigate("/interview", {state: '1'});
+  };
+  const goToClose = () => {
+    navigate("/interview", {state: '3'});
+  };
   return (
     <div>
       <Grid container spacing={2}>
@@ -34,7 +34,7 @@ export default function BasicCard() {
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Learn More</Button>
+              <Button size="small" onClick={goToMatch}>더보기</Button>
             </CardActions>
           </Card>
         </Grid>
@@ -49,7 +49,7 @@ export default function BasicCard() {
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Learn More</Button>
+              <Button size="small" onClick={goToApply}>더보기</Button>
             </CardActions>
           </Card>
         </Grid>
@@ -64,7 +64,7 @@ export default function BasicCard() {
               </Typography>
             </CardContent>
             <CardActions>
-              <Button size="small">Learn More</Button>
+              <Button size="small" onClick={goToClose}>더보기</Button>
             </CardActions>
           </Card>
         </Grid>
