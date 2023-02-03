f5f3c6dbe161d4d23e55a5ee9b9f767bfed39f94
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-02-03 12:43:59 +0900

    <feat> speach to text 파일 합치기

--- frontend/src/components/conference/AnswerWrite.jsx
+++ frontend/src/components/conference/AnswerWrite.jsx
@@ -1,21 +1,34 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
+// import TextField from "@mui/material/TextField";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
 
 export default function AnswerWrite(props) {
-  const [name, setName] = React.useState("");
-  const handleChange = (event) => {
-    setName(event.target.value);
-    console.log(name);
-  };
+  // const [name, setName] = React.useState("");
+  // const handleChange = (event) => {
+  //   setName(event.target.value);
+  //   console.log(name);
+  // };
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+  const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'ko' });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
 
   return (
-    <TextField
-      id="outlined-multiline-static"
-      multiline
-      rows={10}
-      fullWidth
-      value={name}
-      onChange={handleChange}
-    />
+    <div>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button onClick={startListening}>Start</button>
+      <button onClick={SpeechRecognition.stopListening}>Stop</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <p>{transcript}</p>
+    </div>
   );
 }
