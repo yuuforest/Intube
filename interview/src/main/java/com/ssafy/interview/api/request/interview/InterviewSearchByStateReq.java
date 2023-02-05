4ef8fca45a3a1725b2b9c40775d78eae645ec648
jangyejoo <jyj01068662778@gamil.com>
2023-02-05 18:31:22 +0900

    <build> : builder.default 삭제

--- interview/src/main/java/com/ssafy/interview/api/request/interview/InterviewSearchByStateReq.java
+++ interview/src/main/java/com/ssafy/interview/api/request/interview/InterviewSearchByStateReq.java
@@ -16,7 +16,6 @@ public class InterviewSearchByStateReq {
     @ApiModelProperty(example = "", value = "인터뷰 상태 - 4(모집), 5(진행), 6(완료)")
     private int interview_state;
 
-    @Builder.Default
     @ApiModelProperty(example = "", value = "검색어(공고 제목 or 내용)")
     private String word = null;
 }
--- interview/src/main/java/com/ssafy/interview/api/service/user/AuthServiceImpl.java
+++ interview/src/main/java/com/ssafy/interview/api/service/user/AuthServiceImpl.java
@@ -144,7 +144,7 @@ public class AuthServiceImpl implements AuthService {
 
         // 2. 쿠키에 저장 - response header 넣어서 보냄
         ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
-                .domain("i8a303.p.ssafy.io") // 쿠키 도메인 설정
+                .domain("52.78.72.74") // 쿠키 도메인 설정
                 .maxAge(expireTime)
                 .path("/")
                 .secure(true)
