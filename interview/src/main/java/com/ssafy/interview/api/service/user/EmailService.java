cd3dea9ce6da51fe929406a6366473759e4b0c98
jangyejoo <jyj01068662778@gamil.com>
2023-02-06 10:54:52 +0900

    <build> : interview 삭제

--- interview/src/main/java/com/ssafy/interview/api/service/user/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.interview.api.service.user;
-
-public interface EmailService {
-    String sendAuthCode(String to) throws Exception;
-    String sendRandomPassword(String to) throws Exception;
-}
