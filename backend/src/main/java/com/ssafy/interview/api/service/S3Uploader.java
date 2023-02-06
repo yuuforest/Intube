9f0c4d9503d605a738e569abf61725936110a5b9
jangyejoo <jyj01068662778@gamil.com>
2023-02-06 10:43:47 +0900

    <build> : 디렉토리 구조 변경

--- backend/src/main/java/com/ssafy/interview/api/service/S3Uploader.java
+++ backend/src/main/java/com/ssafy/interview/api/service/S3Uploader.java
@@ -2,6 +2,7 @@ package com.ssafy.interview.api.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Optional;
@@ -25,16 +27,21 @@ public class S3Uploader {
     @Value("${cloud.aws.s3.bucket}")
     private String S3Bucket;
 
-    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadFile = convert(multipartFile).orElseThrow(() -> new IllegalArgumentException("error : MultipartFile -> file convert fail"));
-        return upload(uploadFile, dirName);
-    }
+//    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+//        File uploadFile = convert(multipartFile).orElseThrow(() -> new IllegalArgumentException("error : MultipartFile -> file convert fail"));
+//        return upload(uploadFile, dirName);
+//    }
 
     // S3로 파일 업로드하기
-    private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + UUID.randomUUID() + uploadFile.getName();  // S3에 저장된 파일 이름
+    public String upload(MultipartFile uploadFile, String dirName) throws IOException {
+        String ext = uploadFile.getOriginalFilename().substring(uploadFile.getOriginalFilename().lastIndexOf('.') + 1);
+        String o_FileName = uploadFile.getOriginalFilename().substring(uploadFile.getOriginalFilename().lastIndexOf('.'));
+        String StrToday = (new SimpleDateFormat("yyyyMMddHHmmss").format(Calendar.getInstance().getTime()));
+        String strFileName = "_" + StrToday + "." + ext;
+
+        String fileName = dirName + "/" + UUID.randomUUID() + strFileName;  // S3에 저장된 파일 이름
+
         String uploadImageUrl = putS3(uploadFile, fileName); // s3로 업로드
-        removeNewFile(uploadFile);
         return fileName;
     }
 
@@ -46,8 +53,15 @@ public class S3Uploader {
     }
 
     // S3로 업로드
-    private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(S3Bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+    private String putS3(MultipartFile uploadFile, String fileName) throws IOException {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(uploadFile.getContentType());
+        objectMetadata.setContentLength(uploadFile.getSize());
+
+        InputStream inputStream = uploadFile.getInputStream();
+        amazonS3Client.putObject(new PutObjectRequest(S3Bucket, fileName, inputStream, objectMetadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+
         String result = amazonS3Client.getUrl(S3Bucket, fileName).toString();
         return result;
     }
@@ -57,7 +71,7 @@ public class S3Uploader {
         String ext = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.') + 1);
         String o_FileName = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf('.'));
         String StrToday = (new SimpleDateFormat("yyyyMMddHHmmss").format(Calendar.getInstance().getTime()));
-        String strFileName = "_"+StrToday+"."+ext;
+        String strFileName = "_" + StrToday + "." + ext;
 
         File convertFile = new File(System.getProperty("user.dir") + "/" + strFileName);
         if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
@@ -68,4 +82,5 @@ public class S3Uploader {
         }
         return Optional.empty();
     }
+
 }
