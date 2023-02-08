9420a97ac32726c8e496b1f1c973599911046e11
dlgkssk38 <dlgkssk38@snu.ac.kr>
2023-02-08 15:56:23 +0900

    <feat>: 질문자 마이페이지 BE 연결 및 components 오버라이딩 된 것 해결

--- frontend/src/components/answerer/mypage/CalendarComponent.jsx
+++ frontend/src/components/answerer/mypage/CalendarComponent.jsx
@@ -1,42 +1,42 @@
-import React, { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import moment from "moment";
-import "./CalendarComponent.css";
-import MatchList from "./MatchList.jsx";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import moment from "moment"
+// import GetList from "../../../json/answerer_list_state2.json";
+import './CalendarComponent.css'
+import MatchList from './MatchList.jsx';
 
-export default function CalendarComponent() {
-  const GetList = null;
+export default function CalendarComponent(props) {
   const [value, onChange] = useState(new Date());
-  const interviewList = GetList.interview_list;
+  const interviewList = props.calendarInfo
+
 
   return (
     <div
     // style={{float:'right', margin:'0 200px'}}
     >
       <Calendar
-        id="calendar"
-        onChange={onChange}
-        value={value}
-        formatDay={(locale, date) => moment(date).format("DD")}
-        navigationLabel={null}
-        showNeighboringMonth={false}
-        tileClassName={({ date, view }) => {
-          if (
-            interviewList.find(
-              (x) =>
-                x.interview_time.slice(0, 10) ===
-                moment(date).format("YYYY-MM-DD")
-            )
-          ) {
-            return "highlight";
-          }
-        }}
+      id="calendar"
+      onChange={onChange}
+      value={value}
+      formatDay={(locale, date) => moment(date).format("DD")}
+      navigationLabel={null}
+      showNeighboringMonth={false}
+      tileClassName={({ date, view }) => {
+        if (interviewList.find((x) => 
+        x.interview_start_time.slice(0,10) === moment(date).format("YYYY-MM-DD"))) {
+          return "highlight";
+        }
+      }}
       />
-      <div>
-        <p>{moment(value).format("YYYY년 MM월 DD일")}</p>
-        <MatchList date={moment(value).format("YYYY-MM-DD")} />
-      </div>
+         <div>
+           <p>{moment(value).format("YYYY년 MM월 DD일")}</p>
+           <MatchList 
+           position={props.position}
+           matchInfo={interviewList.filter(
+            (x) => x.interview_start_time.slice(0,10) === moment(value).format("YYYY-MM-DD"))} 
+           />
+         </div>
     </div>
   );
 }
--- frontend/src/components/answerer/mypage/MatchListItem.jsx
+++ frontend/src/components/answerer/mypage/MatchListItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import InterviewListItemDetail from "components/main/interview/MainInterviewListItemDetail";
+// import InterviewListItemDetail from "components/main/interview/MainInterviewListItemDetail";
+import AnswererListItemDetail from '../AnswererListItemDetail'
 
 export default function MatchListItem(props) {
   const [open, setOpen] = React.useState(false);
@@ -11,13 +12,24 @@ export default function MatchListItem(props) {
   return (
     <div>
       <p onClick={handleClickOpen}>
-        {props.interview.interview_time.slice(11)}: {props.interview.title}
+        {props.interview.interview_start_time.slice(11)}: {props.interview.title}
       </p>
-      <InterviewListItemDetail
+      {
+        props.position === 1
+        ?
+        <AnswererListItemDetail
         open={open}
         setOpen={setOpen}
         interview={props.interview}
-      />
+        />
+        :
+        // 이 부분 질문자 관련된 modal로 수정하기!
+        <AnswererListItemDetail
+          open={open}
+          setOpen={setOpen}
+          interview={props.interview}
+        />
+      }
     </div>
   );
 }
