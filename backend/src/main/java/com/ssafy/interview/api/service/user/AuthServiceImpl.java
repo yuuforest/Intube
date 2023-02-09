a97e51b07a6663868220d059f08486d453427d10
jangyejoo <jyj01068662778@gamil.com>
2023-02-09 10:53:58 +0900

    <fix> : 카카오 https->http, 쿠키 도메인 변경

--- backend/src/main/java/com/ssafy/interview/api/service/user/AuthServiceImpl.java
+++ backend/src/main/java/com/ssafy/interview/api/service/user/AuthServiceImpl.java
@@ -59,7 +59,7 @@ public class AuthServiceImpl implements AuthService {
             MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
             body.add("grant_type", "authorization_code");
             body.add("client_id", "dc6c7559412fd1c77ad3e0a798803e27");
-            body.add("redirect_uri", "https://intube.store:8443/signup");
+            body.add("redirect_uri", "http://intube.store:8443/signup");
             body.add("code", code);
 
             // HTTP 요청 보내기
@@ -149,7 +149,7 @@ public class AuthServiceImpl implements AuthService {
 
         // 2. 쿠키에 저장 - response header 넣어서 보냄
         ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
-                .domain("i8a303.p.ssafy.io")
+                .domain("intube.store")
                 .maxAge(refreshExpireTime)
                 .path("/")
                 .secure(true)
