e5dadaf09a61ffe049808043699baea779c8e98b
yuuforest <yurimkang20@gmail.com>
2023-02-09 16:07:59 +0900

    <feat> : Conference 생성 시 중복 체크 기능 추가

--- backend/src/main/java/com/ssafy/interview/db/repository/conference/ConferenceRepository.java
+++ backend/src/main/java/com/ssafy/interview/db/repository/conference/ConferenceRepository.java
@@ -4,6 +4,10 @@ import com.ssafy.interview.db.entitiy.conference.Conference;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConferenceRepository extends JpaRepository<Conference, Long> {
+
+    Optional<Conference> findByInterviewTime_Id(Long interview_time_id);
 }
