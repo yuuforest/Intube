f984a2bab813723c66178d3a8efe77da9622ce4f
slhyj95 <slhyj95@naver.com>
2023-02-09 16:06:25 +0900

    <feat> : interview 마감 수정

--- backend/src/main/java/com/ssafy/interview/db/entitiy/conference/ConferenceResult.java
+++ backend/src/main/java/com/ssafy/interview/db/entitiy/conference/ConferenceResult.java
@@ -2,18 +2,24 @@ package com.ssafy.interview.db.entitiy.conference;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ssafy.interview.db.entitiy.BaseEntity;
+import com.ssafy.interview.db.entitiy.interview.Interview;
 import com.ssafy.interview.db.entitiy.interview.Question;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.util.Assert;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class ConferenceResult extends BaseEntity {
 
     String content;
@@ -27,4 +33,10 @@ public class ConferenceResult extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id")
     private Question question;
+
+    @Builder
+    private ConferenceResult(String content) {
+        Assert.hasText(content, "content must not be empty");
+        this.content = content;
+    }
 }
