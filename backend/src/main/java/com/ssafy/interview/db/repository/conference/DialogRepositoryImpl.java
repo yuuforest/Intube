4aa2ba7735b3f7fd1cf70bdc2206134750957bba
YoungJun Lee <slhyj95@gmail.com>
2023-02-13 23:05:13 +0900

    <feat> : 인터뷰 결과 Dialog방식 조회 완료

--- backend/src/main/java/com/ssafy/interview/db/repository/conference/DialogRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/interview/db/repository/conference/DialogRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.ssafy.interview.db.repository.conference;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.interview.api.response.result.DialogDetailRes;
+import com.ssafy.interview.api.response.result.QDialogDetailRes;
 import com.ssafy.interview.db.entitiy.QUser;
 import com.ssafy.interview.db.entitiy.conference.Dialog;
 import com.ssafy.interview.db.entitiy.conference.QDialog;
+import com.ssafy.interview.db.entitiy.interview.QQuestion;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -13,6 +16,7 @@ public class DialogRepositoryImpl implements DialogRepositoryCustom {
     private JPAQueryFactory jpaQueryFactory;
     QDialog qDialog = QDialog.dialog;
     QUser qUser = QUser.user;
+    QQuestion qQuestion = QQuestion.question;
 
     @Override
     public List<Dialog> findAllByConferenceId(Long conference_id) {
@@ -20,6 +24,18 @@ public class DialogRepositoryImpl implements DialogRepositoryCustom {
                 .select(qDialog)
                 .from(qDialog)
                 .leftJoin(qDialog.user, qUser).fetchJoin()
+                .where(qDialog.conference.id.eq(conference_id))
+                .orderBy(qDialog.timestamp.asc())
+                .fetch();
+    }
+
+    @Override
+    public List<DialogDetailRes> findDialogDetailRes(Long conference_id) {
+        return jpaQueryFactory
+                .select(new QDialogDetailRes(qDialog, qQuestion, qUser))
+                .from(qDialog)
+                .leftJoin(qDialog.user, qUser)
+                .leftJoin(qDialog.question, qQuestion)
                 .where(qDialog.conference.id.eq(conference_id))
                 .orderBy(qDialog.timestamp.asc())
                 .fetch();
